004904a6f4978ba37b02a4e4f84456854973106d
zackhengartner <zhengar@rockets.utoledo.edu>
2026-02-08 17:46:25 -0500

    trying again

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import picofme from './assets/me.png';
 import pythonLogo from './assets/pythonLogo.png';
 import javaLogo from './assets/javaLogo.png';
 import cLogo from './assets/cLogo.png';
-import cplusplusLogo from './assets/c++Logo.png';
+import cplusplusLogo from './assets/cplusplusLogo.png';
 import htmlLogo from './assets/htmlLogo.png';
 import cssLogo from './assets/cssLogo.png';
 
